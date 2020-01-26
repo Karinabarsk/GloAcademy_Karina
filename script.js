4df3d53fcb701657f4dc75dbd1e7eebe8610523a
Karina Yakimovich <karina.jakimovich@gmail.com>
2020-01-27 00:28:21 +0200

    урок второй

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+let money = 5000, 
+    income = 'фриланс', 
+    addExpenses = 'коммуналка',  
+    deposit = false, 
+    mission = 20000, 
+    period = 6;
+
+alert ('Hello world');
+
+console.log('Hello world 2');
+
+console.log (typeof money, typeof income, typeof deposit),
+
+console.log (addExpenses.length);
+
+console.log('Период равен:', period, 'месяцев' );
+console.log ('Цель заработать', mission, 'euro');
+
+addExpenses.toLowerCase;
+console.log(addExpenses.split());
+
+let budgetDay = money/30;
+console.log(budgetDay);
+
