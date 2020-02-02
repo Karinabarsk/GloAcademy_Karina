9263e4c3600fd495e0bd43d089e4533b2cf2ecaf
Karina Yakimovich <karina.jakimovich@gmail.com>
2020-02-02 15:56:43 +0200

    ok

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 'use strict';
 
-let arr = ["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"];
+let week = ["понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"];
+let toDay = new Date().getDay() - 1;
 
-let am = arr[6].italics();
+/*let am = arr[6].italics();
 let bm = arr[5].italics();
 let cm = arr[5].bold();
 
 arr.splice(5, 2);
 arr.push(cm, am);
-document.write(arr.join('<br \/>'));
+document.write(arr.join('<br \/>'));*/
 
+
+for (let i = 0; i < week.length; i++) {
+    if (i === toDay) {
+     if (week[i] === 'суббота' || week[i] === 'воскресенье') {
+       document.write(week[i].bold().italics());
+       } else {
+         document.write(week[i].bold());
+       }
+     } else if (week[i] === 'суббота' || week[i] === 'воскресенье') {
+       document.write(`<p>${week[i].bold().italics()}</p>`);
+     } else {
+       document.write(`<p>${week[i]}</p>`);
+     }
+   }
+   console.log(week);
