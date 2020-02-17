1721592ef1bed5ec8fc2e5dfa77b68a4f1eb2e99
Karina Yakimovich <karina.jakimovich@gmail.com>
2020-02-17 12:24:13 +0200

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+
+class First {
+    hello(){
+      console.log('Привет я метод родителя!');
+    }
+  }
+  
+  class Second extends First{
+    hello() {
+      super.hello();
+      console.log('А я наследуемый метод!');
+    }
+  }
+  
+  let second = new Second();
+  second.hello();
+
+
