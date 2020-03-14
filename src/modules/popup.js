50a10480246e4c05a4e4f62bd619383487295961
Karina Yakimovich <karina.jakimovich@gmail.com>
2020-03-14 12:55:33 +0200

    sendForm

---
+++ src/modules/popup.js
@@ -0,0 +1,28 @@
+const popup = () => {
+    
+/*const callBtn = document.getElementsByClassName ('call-btn');
+
+    document.addEventListener ('click', (event) => {
+        let target = event.target;
+        if (target = target.closest('.popup')) {
+         callBtn.classList.add('popup-call');
+      } else {
+            target = event.target;
+            if (callBtn.classList.contains('popup-call')) {
+               if (target !== target.closest('.popup-call')) {
+                  callBtn.classList.remove('popup-call');
+               } else {
+                  if (target !== target.closest('.popup-call')) {
+                     callBtn.classList.remove('popup-call');
+                  }
+               }
+            }
+            return;
+         }
+      });*/
+
+      alert('Hello');
+
+};
+
+export default popup;
